2e92885ce73098609a094833e7ef24306f07445b
sumayyaibrahim <132639444+sumayyaibrahim@users.noreply.github.com>
2023-05-05 11:08:34 +0530

    Initial commit

---
+++ basic1.js
@@ -0,0 +1,36 @@
+ var a=3;
+
+{  
+    let a=6;
+    console.log(a);
+}
+
+console.log(a);
+a=[1,2,3,"acd"]
+console.log(a.length);
+let person={
+    name:'sumayya',
+    age:24,
+
+
+}
+console.log(person);
+console.log(person.name);
+console.log("welcome");
+function myFunctions()
+{
+
+}
+
+let i=5;
+do{
+    console.log('hello $i');
+
+
+}while(i>2)
+
+
+
+
+
+
